10df82f09877bf42f66c80e1b96b493b3ff80421
Nicolas <nicolaspulido1979@gmail.com>
2024-01-11 11:34:37 -0500

    Implementando JWT

--- servidorExpress.js
+++ servidorExpress.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = 8000;
 
@@ -17,18 +18,76 @@ let listaDeTareas = [
 
 app.use(express.json());
 
-app.post('/crearTarea', (req, res) => {
-  const nuevaTarea = req.body; 
+// Middleware global de verificación de token
+app.use((req, res, next) => {
+  const beareHeader = req.headers["authorization"];
+
+  if (typeof beareHeader !== "undefined") {
+    const bearerToken = beareHeader.split(" ")[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Bienvenido sergio"
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 1,
+    nombre: "Nicolas",
+    email: "nicolas@gmail.com"
+  }
+  jwt.sign({ user }, "secreto", { expiresIn: "32s" }, (err, token) => {
+    res.json({
+      token
+    })
+  })
+})
+
+app.post("/api/post", (req, res) => {
+  jwt.verify(req.token, "secreto", (error, authData) => {
+    if (error) {
+      res.json({
+        message: "No tienes acceso"
+      });
+    } else {
+      res.json({
+        message: "Post fue creado",
+        authData
+      })
+    }
+  })
+});
+
+// Aplicar el middleware de verificación del token a todas las rutas siguientes
+app.use((req, res, next) => {
+  jwt.verify(req.token, "secreto", (error, authData) => {
+    if (error) {
+      res.status(403).json({ message: "No tienes acceso" });
+    } else {
+      next();
+    }
+  });
+});
+
+app.post('/api/crearTarea', (req, res) => {
+  const nuevaTarea = req.body;
   listaDeTareas.push(nuevaTarea);
   res.status(201).json({ message: 'Tarea agregada correctamente' });
 });
 
-app.get('/verTareas', (req, res) => {
+app.get('/api/verTareas', (req, res) => {
   res.status(200).json(listaDeTareas);
 });
 
-app.delete('/borrar/:id', (req, res) => {
-  const idABorrar = req.params.id; 
+app.delete('/api/borrar/:id', (req, res) => {
+  const idABorrar = req.params.id;
   const tareaBorrada = listaDeTareas.find(tarea => tarea.id === idABorrar);
 
   if (!tareaBorrada) {
@@ -36,16 +95,16 @@ app.delete('/borrar/:id', (req, res) => {
   }
 
   listaDeTareas = listaDeTareas.filter(tarea => tarea.id !== idABorrar);
-  res.status(200).json({ message: 'La tarea se elimino', tareaBorrada });
+  res.status(200).json({ message: 'La tarea se eliminó', tareaBorrada });
 });
 
-app.put('/actualizarTarea/:id', (req, res) => {
-  const id = req.params.id; 
+app.put('/api/actualizarTarea/:id', (req, res) => {
+  const id = req.params.id;
   let tareaCompletada = listaDeTareas.find(tarea => tarea.id === id);
 
   if (tareaCompletada) {
     tareaCompletada.estaCompletada = true;
-    res.status(200).json({ message: "la tarea se actualizo" });
+    res.status(200).json({ message: "La tarea se actualizó" });
   } else {
     res.status(404).json({ message: "Tarea no encontrada" });
   }
@@ -54,5 +113,3 @@ app.put('/actualizarTarea/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
-
-
